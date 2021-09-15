7555673ea0d3741223ee261929583b5740185147
prathamFlobiz <pratham@flobiz.in>
2021-09-15 20:46:06 +0530

    Added timer for Splash Activity, added vector assest and created style for Splash Activity

--- app/src/main/java/com/example/event_reminder/SplashActivity.kt
+++ app/src/main/java/com/example/event_reminder/SplashActivity.kt
@@ -1,26 +1,43 @@
 package com.example.event_reminder
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.WindowManager
+import java.util.*
+import java.util.TimerTask
+
+
+
+
 
 class SplashActivity : AppCompatActivity() {
+    //Timer for starting MainActivity
+    var timer = Timer()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
-
-
-        Handler().postDelayed({
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish()
-        }, 5000)
+        startTimer()
+    }
+    override fun onPause() {
+        super.onPause()
+        timer.cancel()
+    }
+    fun launchMainActivity(){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+    private fun startTimer(){
+        val timerTask: TimerTask = object : TimerTask() {
+            override fun run() {
+                launchMainActivity()
+            }
+        }
+        timer.schedule(timerTask,5000)
     }
 }
