e7874db39f8eaddb3a53a00d94503c0894ed9668
prathamFlobiz <pratham@flobiz.in>
2021-09-16 15:37:09 +0530

    handler for Splash Activity

--- app/src/main/java/com/example/event_reminder/SplashActivity.kt
+++ app/src/main/java/com/example/event_reminder/SplashActivity.kt
@@ -1,43 +1,33 @@
 package com.example.event_reminder
 
-import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import android.view.WindowManager
-import java.util.*
-import java.util.TimerTask
-
-
-
+import androidx.appcompat.app.AppCompatActivity
 
 
 class SplashActivity : AppCompatActivity() {
-    //Timer for starting MainActivity
-    var timer = Timer()
+    //Handler variables
+    private val handler = Handler()
+    private var runnable: Runnable? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-
-        startTimer()
+        runnable = Runnable {
+                launchMainActivity()
+        }
+        runnable?.let {
+            handler.postDelayed(it,5000)
+        }
     }
     override fun onPause() {
+        handler.removeCallbacksAndMessages(null)
         super.onPause()
-        timer.cancel()
     }
-    fun launchMainActivity(){
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
+    private fun launchMainActivity(){
+        startActivity(Intent(this, MainActivity::class.java))
         finish()
     }
-    private fun startTimer(){
-        val timerTask: TimerTask = object : TimerTask() {
-            override fun run() {
-                launchMainActivity()
-            }
-        }
-        timer.schedule(timerTask,5000)
-    }
+
 }
